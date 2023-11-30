2e42735540c4592594db806bea534d96fc0492f4
Chris Durden <cdurden@gmail.com>
2023-11-30 03:45:12 -0600

    Added ChartJS plot

--- ServiceLoader.js
+++ ServiceLoader.js
@@ -3,7 +3,9 @@ import { loadService } from "./lib/common.js";
 function init(paramsMap, parentServices) {
     return Promise.all(
         paramsMap.get("services").map(function (serviceName) {
-            return loadService(serviceName).then(function (serviceModule) {
+            return loadService(serviceName, {
+                baseURL: paramsMap.get("baseURL"),
+            }).then(function (serviceModule) {
                 return serviceModule
                     .init(paramsMap, parentServices)
                     .then(function (updateService) {
